d0813b5f80837fc7577dc9212c49e5d348cf4143
Kylehui611 <96867356+Kylehui611@users.noreply.github.com>
2023-02-21 23:35:45 +0000

    Update Testfile.py

--- theSettlers/Testfile.py
+++ theSettlers/Testfile.py
@@ -12,27 +12,73 @@ screen.fill(background_colour)
 
 #transparent = (0, 0, 0, 0)
 
+carImg = pygame.image.load('hexagonCurve.png')
 
-#carImg = pygame.image.load('hexagonCurve.png')
+carImg = pygame.transform.scale(carImg, (90,90))
 
-#carImg = pygame.transform.scale(carImg, (90,90))
+def car(x,y):
+    screen.blit(carImg, (x,y))
 
-
-#def car(x,y):
-#    screen.blit(carImg, (x,y))
-
-#x =  (width * 0.10)
-#y = (height * 0.10)
+x =  (width * 0.10)
+y = (height * 0.10)
 
 
-#car(x,y)
+car(x,y)
 
 #carImg.image.fill(transparent)
 
 #Opens window
-pygame.display.update()
+#pygame.display.update()
 
+def shuffleHexagonTiles():
+    hexagonTiles = []
 
+    #Number of each hexagon tiles in the game
+    desert = 1
+    mountains = 3
+    hills = 3
+    fields = 4
+    pasture = 4
+    forest = 4
+
+    #how many different types of hexagons there are
+    hexagonNumbers = [0,1,2,3,4,5]
+
+    while deserts + mmountains + hills + fields + pasture + forest == 19
+        hexPlacement = random.choice(hexagonNumbers)
+
+        match pick:
+            case 0:
+                hexagonTiles.append(HexTiles.Desert)
+                Desert -= 1
+                if Desert == 0:
+                    del hexagonTiles[hexagonTiles.index(0)]
+            case 1:
+                hexagonTiles.append(HexTiles.Mountains)
+                Mountains -= 1
+                if Mountains == 0
+                    del hexagonTiles[hexagonTiles.index(1)]
+            case 2:
+                hexagonTiles.append(HexTiles.Hills)
+                Hills -= 1
+                if Hills == 0
+                    del hexagonTiles[hexagonTiles.index(2)]
+            case 3:
+                hexagonTiles.append(HexTiles.Fields)
+                Fields -= 1
+                if Fields == 0
+                    del hexagonTiles[hexagonTiles.index(3)]
+            case 4:
+                hexagonTiles.append(HexTiles.Pasture)
+                Pasture -= 1
+                if Pasture == 0
+                    del hexagonTiles[hexagonTiles.index(4)]
+            case 5:
+                hexagonTiles.append(HexTiles.Forest)
+                Forest -= 1
+                if Forest == 0
+                    del hexagonTiles[hexagonTiles.index(5)]
+        return hexagonNumbers
 
 
 #Keeps screen running and open
@@ -43,3 +89,4 @@ while running:
       running = False
 
 game = GameRunner(3)
+
